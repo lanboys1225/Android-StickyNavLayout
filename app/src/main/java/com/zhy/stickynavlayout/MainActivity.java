f9edae2ec7815d1018055c7e2230eb6427132d25
lanboys1225 <lanboys1225@gmail.com>
2018-05-22 10:26:33 +0800

    找回原生控件控制嵌套滚动控件

--- app/src/main/java/com/zhy/stickynavlayout/MainActivity.java
+++ app/src/main/java/com/zhy/stickynavlayout/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
 
         initViews();
         initDatas();
